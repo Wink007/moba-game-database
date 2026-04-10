9fe37db4cf4695b403143901f3eefaf1653ad4f2
Oleksandr Klievtsov <oleksandr.klievtsov@vista.com>
2026-04-10 10:19:57 +0300

    feat: enable PlayersPage (FF_PLAYERS=true)

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const API_URL = process.env.REACT_APP_API_URL || 'https://api.mobawiki.com/api';
 
 export const FF_SOCIAL = true;
-export const FF_PLAYERS = false;
+export const FF_PLAYERS = true;
